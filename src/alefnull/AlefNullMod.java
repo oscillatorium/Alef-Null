431fa185ffbb21505d24e7665e4c5e0c5a84c8b5
Oscillatorium <oskaradamgold1@gmail.com>
2026-05-10 14:46:48 +0300

    Create src/alefnull/AlefNullMod.java
    воздуханчик

---
+++ src/alefnull/AlefNullMod.java
@@ -0,0 +1,10 @@
+package alefnull;
+
+import mindustry.mod.Mod;
+
+public class AlefNullMod extends Mod {
+    @Override
+    public void init() {
+        System.out.println("Alef-Null mod загружен!");
+    }
+}
