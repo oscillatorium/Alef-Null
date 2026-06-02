d0e0b982bfbb709ec0509e0e2308f5d9842e01c1
Oscillatorium <oskaradamgold1@gmail.com>
2026-06-02 17:38:32 +0300

    Update main.js
    обрыганчик делает ээээээээнергию

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-print("Alef-Null: script loaded");
+const CORE_NAME = "core-n";
+const POWER_PER_TICK = 500 / 60;
+
+Events.on(ClientLoadEvent, () => {
+    print("scripts activated");
+});
+
+Events.run(Trigger.update, () => {
+    let cores = Vars.state.teams.playerCores();
+    for (let i = 0; i < cores.size; i++) {
+        let coreBuild = cores.get(i);
+        if (coreBuild && coreBuild.block.name === CORE_NAME) {
+            coreBuild.items.add(Items.power, POWER_PER_TICK);
+        }
+    }
+});
