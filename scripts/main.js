35b9b8a31d66cc7ac8b163e65515484d4b298cfd
Oscillatorium <oskaradamgold1@gmail.com>
2026-06-02 17:06:17 +0300

    Create main.js
    🤺🦸🕴️🕺🤾

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const CORE_NAME = "core-n";
+const POWER_PER_TICK = 500 / 60;
+
+Events.run(Trigger.update, () => {
+    let cores = Vars.state.teams.playerCores();
+    for (let i = 0; i < cores.size; i++) {
+        let coreBuild = cores.get(i);
+        if (coreBuild && coreBuild.block.name === CORE_NAME) {
+            coreBuild.items.add(Items.power, POWER_PER_TICK);
+        }
+    }
+});
