51420e3e662d1133223f21ba486536b6b6ed7626
Oscillatorium <oskaradamgold1@gmail.com>
2026-06-03 13:09:00 +0300

    Update main.js
    обрыганчик делает ээээээээнергию изо всех сиыыыыыыыыил

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,24 @@
 const CORE_NAME = "core-n";
 const POWER_AMOUNT = 500;
 
-let generator = null;
-
-Events.on(ClientLoadEvent, () => {
-    print("scripts activated");
-});
+let generators = {};
 
 Events.run(Trigger.update, () => {
     let cores = Vars.state.teams.playerCores();
     for (let i = 0; i < cores.size; i++) {
         let coreBuild = cores.get(i);
         if (coreBuild && coreBuild.block.name === CORE_NAME) {
-            if (!generator) {
-                generator = new PowerGraph();
+            if (!generators[coreBuild.id]) {
+                generators[coreBuild.id] = true;
+                coreBuild.consumesPower = true;
+                coreBuild.outputsPower = true;
+                coreBuild.power.graph.add(POWER_AMOUNT / 60);
             }
-            generator.add(POWER_AMOUNT / 60);
-            coreBuild.power.graph = generator;
+            coreBuild.power.graph.add(POWER_AMOUNT / 60);
         }
     }
+});
+
+Events.on(ClientLoadEvent, () => {
+    print("scripts activatededed");
 });
