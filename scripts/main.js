2f3f95adc11f187e4c05dfdd8e46e4d6cdc7ca33
Oscillatorium <oskaradamgold1@gmail.com>
2026-06-04 19:58:56 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,1 @@
-// Расширенность для побочность генераторноость ноль
-const generatorDecay = extend(ConsumeGenerator, "generator-decay", {
-    update(tile) {
-        this.super$update(tile);
-        
-        let entity = tile.ent();
-        
-        if (entity.warmup > 0.99 && entity.timer.get(0, 60)) {
-            if (entity.items.get(itemMap.zero) < this.itemCapacity) {
-                entity.items.add(itemMap.zero, 5);
-            }
-        }
-    }
-});
+// Расширенность для побочность генераторность ноль
