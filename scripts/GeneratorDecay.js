850a3ba4ecc0ccef67c2d24466b44bbbe3bf6ee3
Oscillatorium <oskaradamgold1@gmail.com>
2026-06-04 21:02:08 +0300

    Create GeneratorDecay.js
    Домой, Уолтер

---
+++ scripts/GeneratorDecay.js
@@ -0,0 +1,47 @@
+const GeneratorDecay = {
+
+  ticksPerItem: 12,
+  tickers: {},
+
+  init() {
+    Events.on(EventType.ContentInitEvent, () => {
+      this.generator = Vars.content.getByName(ContentType.block, "alef-null-generator-decay");
+      this.zeroItem  = Vars.content.getByName(ContentType.item, "alef-null-zero");
+      this.registerTick();
+    });
+  },
+
+  registerTick() {
+    Events.run(Trigger.update, () => {
+      if (!Vars.state.isGame()) return;
+
+      Groups.build.each(building => {
+        if (building.block !== this.generator) return;
+
+        const id = building.id;
+        const count = building.items.get(this.zeroItem);
+        const capacity = building.block.itemCapacity;
+
+        //я умни, я научился выводить предметы
+        const canDump = building.dump(this.zeroItem);
+
+        if (count >= capacity && !canDump) {
+          building.enabled = false;
+          this.tickers[id] = 0;
+          return;
+        }
+
+        building.enabled = true;
+
+        this.tickers[id] = (this.tickers[id] || 0) + 1;
+        if (this.tickers[id] < this.ticksPerItem) return;
+        this.tickers[id] = 0;
+
+        building.offload(this.zeroItem);
+      });
+    });
+  }
+
+};
+
+module.exports = GeneratorDecay;
