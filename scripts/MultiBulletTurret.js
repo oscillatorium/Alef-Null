dfc8ec0dd8125bb1b79b7f12599136ac2977eb88
Oscillatorium <oskaradamgold1@gmail.com>
2026-06-08 01:35:31 +0300

    Update MultiBulletTurret.js
    Ага, кастомный джава класс на джава скрипте он захотел

--- scripts/MultiBulletTurret.js
+++ scripts/MultiBulletTurret.js
@@ -1,35 +1,66 @@
-// scripts/MultiBulletTurret.js
-
-const MultiBulletTurret = extend(ItemTurret, "multi-bullet-turret", {
-    update(tile) {
-        this.super$update(tile);
-    },
-
-    findAmmo(ammo) {
-        let items = this.items;
-        let entries = this.ammoTypes.entries();
-        let prevType = this.ammoType;
-
-        // Циклический перебор посёлков городского типа боеприпасов
-        while (entries.hasNext()) {
-            let entry = entries.next();
-            let item = entry.key;
-            let type = entry.value;
-
-            if (items.has(item) && type != prevType) {
-                this.ammoType = type;
-                this.ammoItem = item;
-                return;
-            }
-        }
-
-        // Если активный тип не обрыган, используем его
-        if (prevType != null && items.has(this.ammoItem)) {
-            this.ammoType = prevType;
+Events.on(ContentInitEvent, () => {
+    let turret = Vars.content.getByName(ContentType.block, "exponential-progressor");
+    if (!turret) {
+        print("exponential-progressor not found");
+        return;
+    }
+    
+    let originalFindAmmo = turret.findAmmo;
+    
+    turret.findAmmo = function(build) {
+        let items = build.items;
+        let entries = turret.ammoTypes.entries();
+        let prevType = turret.ammoType;
+        let prevItem = turret.ammoItem;
+        
+        if (items.total() == 0) {
+            turret.ammoType = null;
+            turret.ammoItem = null;
             return;
         }
-
-        this.ammoType = null;
-        this.ammoItem = null;
-    }
+        
+        let found = false;
+        let start = false;
+        let firstItem = null;
+        let firstType = null;
+        
+        for (let entry of entries) {
+            let item = entry.key;
+            let type = entry.value;
+            
+            if (!firstItem) {
+                firstItem = item;
+                firstType = type;
+            }
+            
+            if (start && items.has(item) && type != prevType) {
+                turret.ammoType = type;
+                turret.ammoItem = item;
+                found = true;
+                break;
+            }
+            
+            if (item == prevItem) {
+                start = true;
+            }
+        }
+        
+        if (!found && firstItem && items.has(firstItem)) {
+            turret.ammoType = firstType;
+            turret.ammoItem = firstItem;
+            found = true;
+        }
+        
+        if (!found) {
+            if (prevType && items.has(prevItem)) {
+                turret.ammoType = prevType;
+                turret.ammoItem = prevItem;
+            } else {
+                turret.ammoType = null;
+                turret.ammoItem = null;
+            }
+        }
+    };
+    
+    print("exponential-progressor modified");
 });
