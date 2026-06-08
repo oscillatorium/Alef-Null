9da8eee1192dc84938ffc4aa90e7a2aec4be6c2d
Oscillatorium <oskaradamgold1@gmail.com>
2026-06-08 10:27:13 +0300

    Create ExponentialProgressor.js
    экспорт модуля в унитаз 🤾

---
+++ scripts/ExponentialProgressor.js
@@ -0,0 +1,59 @@
+const ExponentialProgressor = {
+
+  pattern: [2, 4, 8, 16, 32, 64, 128, 256, 512],
+  cooldownTicks: 120,
+
+  indices: {},
+  cooldowns: {},
+  lastShotTime: {},
+
+  init() {
+    Events.on(EventType.ContentInitEvent, () => {
+      this.turret = Vars.content.getByName(ContentType.block, "alef-null-exponential-progressor");
+      if (!this.turret) return;
+
+      // Получаем BulletType снаряда турели
+      this.bulletType = this.turret.ammoTypes.values().toSeq().first();
+
+      this.registerTick();
+    });
+  },
+
+  registerTick() {
+    Events.run(Trigger.update, () => {
+      if (!Vars.state.isGame()) return;
+
+      Groups.build.each(building => {
+        if (building.block !== this.turret) return;
+
+        const id = building.id;
+
+        // Отслеживаем момент выстрела через reloadCounter
+        const justShot = building.reloadCounter <= 0 && (this.lastShotTime[id] || 1) > 0;
+        this.lastShotTime[id] = building.reloadCounter;
+
+        if (justShot) {
+          if ((this.cooldowns[id] || 0) > 0) {
+            this.cooldowns[id]--;
+            this.bulletType.damage = 0;
+            return;
+          }
+
+          const index = this.indices[id] || 0;
+          this.bulletType.damage = this.pattern[index];
+
+          const next = index + 1;
+          if (next >= this.pattern.length) {
+            this.indices[id] = 0;
+            this.cooldowns[id] = this.cooldownTicks;
+          } else {
+            this.indices[id] = next;
+          }
+        }
+      });
+    });
+  }
+
+};
+
+module.exports = ExponentialProgressor;
