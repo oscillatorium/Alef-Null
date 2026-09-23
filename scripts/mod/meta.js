5b9eefd56fe899ca8d24f8c71bc04ee9ad724cee
Oscillatorium <oskaradamgold1@gmail.com>
2026-09-23 11:27:06 +0500

    Create mod/meta.js

---
+++ scripts/mod/meta.js
@@ -0,0 +1,7 @@
+const mod = Vars.mods.locateMod("alef-null").meta;
+
+// Загрузка метаданных из бандлов мода, созданного в качестве прикрытия для пуней математикой
+mod.displayName = Core.bundle.get("mod.alef-null.name");
+mod.author = Core.bundle.get("mod.alef-null.author");
+mod.description = Core.bundle.get("mod.alef-null.description");
+mod.subtitle = Core.bundle.get("mod.alef-null.subtitle");
